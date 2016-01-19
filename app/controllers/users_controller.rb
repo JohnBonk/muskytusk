69a92f2fce243e6d5c614c1a0a94182880849da9
John Bonk <johnabonk@gmail.com>
2016-01-19 16:31:43 -0700

    Updated main display, added Users controller + show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+	def index
+	end
+
+	def show
+		@user = current_user
+		@posts = current_user.posts
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :posts
+  resources :posts, :users
 
   root 'posts#index'
   
