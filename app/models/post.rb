1412f7c53dc3d218de78ae5921b1063abb7efe57
John Bonk <johnabonk@gmail.com>
2016-02-04 21:44:36 -0700

    Update Edit Comment styling, etc

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,8 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
 
-	has_attached_file :image, styles: { large: "", thumb: "100x100>" }, default_url: "/images/:style/missing.png",
-	:storage => :s3,
-  	:bucket => "muskytusk",
-  	:region => "us-west-2",
-  	:url =>':s3_domain_url',
-    :path => '/:class/:attachment/:id_partition/:style/:filename'
+	has_attached_file :image, styles: { large: "", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+
 
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
