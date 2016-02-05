0dbba101342e3d4300ef865cd7507fecdee5e7c7
John Bonk <johnabonk@gmail.com>
2016-02-04 17:43:47 -0700

    Updated prod environment + post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,28 +3,13 @@ class Post < ActiveRecord::Base
 	has_many :comments
 
 	has_attached_file :image, styles: { large: "", thumb: "100x100>" }, default_url: "/images/:style/missing.png",
-	  :storage => :s3,
-  	
+	:storage => :s3,
   	:bucket => "muskytusk",
   	:region => "us-west-2",
   	:url =>':s3_domain_url',
     :path => '/:class/:attachment/:id_partition/:style/:filename'
 
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
-
-  	# This method associates the attribute ":avatar" with a file attachment
-  has_attached_file :avatar, styles: {
-    thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>'
-  }
-
-  # Validate the attached image is image/jpg, image/png, etc
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-
-
-
 
 end
 	
