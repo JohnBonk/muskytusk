ac62031e8e3da5689f95d8c8b6c41109cf1cba99
John Bonk <johnabonk@gmail.com>
2016-02-04 17:18:43 -0700

    Fixed Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,9 +8,21 @@ class Post < ActiveRecord::Base
   	:bucket => "muskytusk",
   	:region => "us-west-2",
   	:url =>':s3_domain_url',
-    :path => '/:class/:attachment/:id_partition/:style/:filename',
+    :path => '/:class/:attachment/:id_partition/:style/:filename'
 
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+
+  	# This method associates the attribute ":avatar" with a file attachment
+  has_attached_file :avatar, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
 
 
 
